0c6af91db5f81008fdd3413818eb71dcea4d4193
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-08-13 20:56:09 +0100

    header bg added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,5 @@ import { Header } from "./components/Header";
 import { NavBar } from "./components/NavBar";
 
 export const App = () => {
-  return (
-    <Box>
-      <Box>
-        <NavBar />
-      </Box>
-      <Header />
-    </Box>
-  );
+  return <Header></Header>;
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,23 @@
 import { Box } from "@mui/system";
+import Typography from "@mui/material/Typography";
 
 export const Header = () => {
-  return <Box>Hello</Box>;
+  return (
+    <Box
+      sx={{
+        height: 800,
+        backgroundColor: "#8338ec",
+      }}
+    >
+      <Typography
+        variant="h1"
+        component="h2"
+        align="center"
+        sx={{ color: "#ffffff" }}
+      >
+        Hello World
+      </Typography>
+      ;
+    </Box>
+  );
 };
