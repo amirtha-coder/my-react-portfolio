879d8355fa77fd1f8412dde979fd728a66daf712
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-08-21 12:24:08 +0100

    Contact me can successfully send

--- src/components/Contact Me.js
+++ src/components/Contact Me.js
@@ -1,23 +1,77 @@
-import * as React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export const ContactMe = () => {
-  const [value, setValue] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (name.length !== 0 && email.length !== 0 && message.length !== 0) {
+      emailjs
+        .sendForm(
+          "service_0duywto",
+          "template_qjchusf",
+          form.current,
+          "nIVIFJXoZ1MufHwIz"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    } else {
+      console.log("error");
+    }
+  };
 
   const handleChange = (event) => {
-    setValue(event.target.value);
+    setOpen(false);
+    if (event.target.name === "user_name") {
+      setName(event.target.value);
+    }
+    if (event.target.name === "user_email") {
+      setEmail(event.target.value);
+    }
+    if (event.target.name === "message") {
+      setMessage(event.target.value);
+    }
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
   };
 
   return (
     <Box
       component="form"
-      method="post"
-      action="mailto:amirtha.m.muthirulandi@gmail.com"
+      ref={form}
+      onSubmit={sendEmail}
       sx={{
         "& .MuiTextField-root": { m: 1, width: "25ch" },
         background:
@@ -37,22 +91,31 @@ export const ContactMe = () => {
         <TextField
           id="filled-multiline-flexible"
           label="Name"
+          name="user_name"
           multiline
           maxRows={6}
-          value={value}
+          value={name}
           onChange={handleChange}
           variant="filled"
         />
         <TextField
           id="filled-textarea"
+          name="user_email"
+          type="text"
+          value={email}
+          onChange={handleChange}
           label="Email"
-          placeholder="Placeholder"
+          placeholder="someone@email.com"
           multiline
           variant="filled"
         />
         <TextField
           id="filled-multiline-static"
+          name="message"
+          value={message}
+          onChange={handleChange}
           label="Message"
+          placeholder="Type here..."
           multiline
           rows={4}
           variant="filled"
@@ -64,9 +127,34 @@ export const ContactMe = () => {
         justifyContent="center"
         spacing={2}
       >
-        <Button variant="contained" type="submit" endIcon={<SendIcon />}>
+        <Button
+          variant="contained"
+          onClick={handleClick}
+          type="submit"
+          value="Send"
+          endIcon={<SendIcon />}
+        >
           Send
         </Button>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          {name.length !== 0 && email.length !== 0 && message.length !== 0 ? (
+            <Alert
+              onClose={handleClose}
+              severity="success"
+              sx={{ width: "100%" }}
+            >
+              This has been successfully sent to creater
+            </Alert>
+          ) : (
+            <Alert
+              onClose={handleClose}
+              severity="error"
+              sx={{ width: "100%" }}
+            >
+              Fill all fields
+            </Alert>
+          )}
+        </Snackbar>
       </Stack>
     </Box>
   );
