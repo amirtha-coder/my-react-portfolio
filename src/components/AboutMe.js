d4184bf1964ceb42ca1839ab6d566179ff2b55a9
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-08-22 18:54:30 +0100

    added components to each page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,45 +4,55 @@ import Typography from "@mui/material/Typography";
 import Typewriter from "typewriter-effect";
 import "../App.css";
 
-import { Stack } from "@mui/system";
+import { Container, Stack } from "@mui/system";
+import { NavBar } from "./NavBar";
+import { Footer } from "./Footer";
 
 export const AboutMe = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        "& > :not(style)": {
-          m: 1,
-          width: "100vw",
-          height: "100%",
-          borderRadius: "5%",
-        },
-      }}
-    >
-      <Box>
-        <Typography variant="h3" component="h4" className="about-me-container">
-          <h2> About Me</h2>
-          <ul>
-            <li>
-              I have volunteered in hospital and been part of explaining impacts
-              of a new technological system introduction to patients. By doing
-              this I have become a strong communuicator- which I know could be
-              overlooked when applying in IT.
-            </li>
-            <li>
-              Through duke of edinburgh, I have gained experience working
-              collabratively which has enhanced my communication abilities and
-              listening skills.
-            </li>
-            <li>
-              I am currently undertaking a coding bootcamp course; prior to
-              this, I have done functional skills qualifications level 1 and 2
-              in IT and an ict technology GCSE at a grade A.
-            </li>
-          </ul>
-        </Typography>
+    <Container>
+      <NavBar />
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: 1,
+            width: "100vw",
+            height: "100%",
+            borderRadius: "5%",
+          },
+        }}
+      >
+        <Box>
+          <Typography
+            variant="h3"
+            component="h4"
+            className="about-me-container"
+          >
+            <h2> About Me</h2>
+            <ul>
+              <li>
+                I have volunteered in hospital and been part of explaining
+                impacts of a new technological system introduction to patients.
+                By doing this I have become a strong communuicator- which I know
+                could be overlooked when applying in IT.
+              </li>
+              <li>
+                Through duke of edinburgh, I have gained experience working
+                collabratively which has enhanced my communication abilities and
+                listening skills.
+              </li>
+              <li>
+                I am currently undertaking a coding bootcamp course; prior to
+                this, I have done functional skills qualifications level 1 and 2
+                in IT and an ict technology GCSE at a grade A.
+              </li>
+            </ul>
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+      <Footer />
+    </Container>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Container } from "@mui/system";
+import { Box, Container } from "@mui/system";
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -6,142 +6,147 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import { NavBar } from "./NavBar";
 
 export const Projects = () => {
   return (
-    <Container
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        flexWrap: "wrap",
-      }}
-    >
-      <Card
+    <Box>
+      <NavBar />
+      <Container
         sx={{
-          maxWidth: 345,
-          margin: "2rem",
-          padding: "1rem",
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
         }}
       >
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            <GitHubIcon fontSize="large" />
-          </Button>
-        </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            <GitHubIcon fontSize="large" />
-          </Button>
-        </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            <GitHubIcon fontSize="large" />
-          </Button>
-        </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            <GitHubIcon fontSize="large" />
-          </Button>
-        </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            <GitHubIcon fontSize="large" />
-          </Button>
-        </CardActions>
-      </Card>
-    </Container>
+        <Card
+          sx={{
+            maxWidth: 345,
+            margin: "2rem",
+            padding: "1rem",
+          }}
+        >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <GitHubIcon fontSize="large" />
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <GitHubIcon fontSize="large" />
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <GitHubIcon fontSize="large" />
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <GitHubIcon fontSize="large" />
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345, margin: "2rem", padding: "1rem" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <GitHubIcon fontSize="large" />
+            </Button>
+          </CardActions>
+        </Card>
+      </Container>
+      <Footer />
+    </Box>
   );
 };
